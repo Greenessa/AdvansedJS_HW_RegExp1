bcee2c736f70ce59e694f6cc93f68ac30fe0f97d
Margo <romanova184@yandex.ru>
2025-04-07 13:34:49 +0300

    Next commit

--- src/js/basic.js
+++ src/js/basic.js
@@ -4,13 +4,16 @@ export class Validator {
   }
   
   validateUsername() {
-    if (/[a-zA-Z]*+\d*+-*+_*/g.test(this.name)) {
+    if (/^\D([a-zA-Z_\-]+\d{0,3}[_]*[\-]*)[a-zA-Z]+$/gm.test(this.name)) {
       console.log('Имя соответствует условиям');
     } else {
       console.log('Необходимо изменить имя пользователя в соответствии с требованиями')
     }
   }
 }
-const hero = new Validator('Pavel-Borisov_134');
+const hero = new Validator('Pavel-135_Borisov');
 hero.validateUsername();
+
+const hero1 = new Validator('Павел-134Борисов');
+hero1.validateUsername();
 
